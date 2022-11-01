44d0ddc2e38ac786a557fee9e75190aa21e3d77d
Inès C <ines.parisdescartes@gmail.com>
2022-11-01 09:32:51 +0100

    full-project

---
+++ src/components/Cards.js
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import Card from './Card'
+
+function Cards() {
+    const [items, setItems] = useState([
+        { id: 1, img: '/img/Benzema.png', stat: "" },
+        { id: 1, img: '/img/Benzema.png', stat: "" },
+        { id: 2, img: '/img/Courtois.png', stat: "" },
+        { id: 2, img: '/img/Courtois.png', stat: "" },
+        { id: 3, img: '/img/Jesus.png', stat: "" },
+        { id: 3, img: '/img/Jesus.png', stat: "" },
+        { id: 4, img: '/img/Kante.png', stat: "" },
+        { id: 4, img: '/img/Kante.png', stat: "" },
+        { id: 5, img: '/img/Kimmich.png', stat: "" },
+        { id: 5, img: '/img/Kimmich.png', stat: "" },
+        { id: 6, img: '/img/Mbappe.png', stat: "" },
+        { id: 6, img: '/img/Mbappe.png', stat: "" },
+        { id: 7, img: '/img/Messi.png', stat: "" },
+        { id: 7, img: '/img/Messi.png', stat: "" },
+        { id: 8, img: '/img/Ronaldo.png', stat: "" },
+        { id: 8, img: '/img/Ronaldo.png', stat: "" },
+    ].sort(() => Math.random() - 0.5))
+
+    const [prev, setPrev] = useState(-1)
+
+    function check(current){
+        if(items[current].id === items[prev].id){
+            items[current].stat = "correct"
+            items[prev].stat = "correct"
+            setItems([...items])
+            setPrev(-1)
+        }else{
+            items[current].stat = "wrong"
+            items[prev].stat = "wrong"
+            setItems([...items])
+            setTimeout(() => {
+                items[current].stat = ""
+                items[prev].stat = ""
+                setItems([...items])
+                setPrev(-1)
+            }, 1000)
+        }
+    }
+
+    function handleClick(id){
+        if(prev === -1){
+            items[id].stat = "active"
+            setItems([...items])
+            setPrev(id)
+        }else{
+            check(id)
+        }
+    }
+
+    return (
+    <div className="container">
+        { items.map((item, index) => (
+            <Card key={index} item={item} id={index} handleClick={handleClick} />
+        )) }
+    </div>)
+}
+
+export default Cards;
